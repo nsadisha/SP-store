da2b21d450a1135d34546c449665356000666fd6
nsadisha <nsadisha@gmail.com>
2022-03-29 01:01:24 +0530

    init

--- app/src/main/java/com/nsadisha/android/spstore/MainActivity.kt
+++ app/src/main/java/com/nsadisha/android/spstore/MainActivity.kt
@@ -1,58 +1,39 @@
 package com.nsadisha.android.spstore
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.nsadisha.android.spstore.databinding.ActivityMainBinding
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+    @SuppressLint("SetTextI18n", "UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val sharedPrefs = getSharedPreferences("SP STORE", Context.MODE_PRIVATE)
 
-        setSupportActionBar(binding.toolbar)
+        val saveBtn = findViewById<Button>(R.id.saveBtn)
+        val showBtn = findViewById<Button>(R.id.showBtn)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val stringInput = findViewById<EditText>(R.id.stringInput)
+        val boolInput = findViewById<Switch>(R.id.boolInput)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        saveBtn.setOnClickListener {
+            val editPrefs = sharedPrefs.edit()
+
+            editPrefs.putString("strValue",stringInput.text.toString().trim())
+            editPrefs.putBoolean("boolValue", boolInput.isChecked)
+
+            editPrefs.apply()
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        showBtn.setOnClickListener {
+            stringInput.setText(sharedPrefs.getString("strValue",""))
+            boolInput.isChecked = sharedPrefs.getBoolean("boolValue", false)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
